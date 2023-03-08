bb9060fbeafc9544f0c5031d482d7e14774383b7
Gabor Halasz <hal2297@gmail.com>
2023-03-08 15:16:58 +0100

    isPrime function created, countPrimes function refactored

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -53,23 +53,23 @@ export class MainPageComponent {
     }
   }
 
-  countPrimes(): any {
-    const primNums: number[] = this.generatedNums.filter((num) => {
-      if (num <= 1) {
+  isPrime(numb: number): boolean {
+    if (numb <= 1) {
+      return false;
+    }
+
+    for (let i = 2; i <= Math.sqrt(numb); i++) {
+      if (numb % i === 0) {
         return false;
       }
+    }
+    return true;
+  }
 
-      for (let i = 2; i <= Math.sqrt(num); i++) {
-        if (num % i === 0) {
-          return false;
-        }
-      }
-      return true;
-    })
+  countPrimes(): any {
+    const primNums: number[] = this.generatedNums.filter((num) => this.isPrime(num))
 
-    const result = new CountPrimes(primNums);
-
-    return result;
+    return new CountPrimes(primNums);;
   }
 
   private displayGeneratedNumbers(nums: number[]) {
